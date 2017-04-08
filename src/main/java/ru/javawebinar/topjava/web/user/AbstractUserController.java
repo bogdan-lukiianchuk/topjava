5f778056e275e2f1ffcfcc0ff3b34d0702cdcce4
Demiurg <demiurgsage@gmail.com>
2017-04-08 13:32:43 +1000

    3 0 2 constructor di

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.web.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
 
@@ -10,40 +12,44 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
+@Controller
 public abstract class AbstractUserController {
     protected final Logger LOG = LoggerFactory.getLogger(getClass());
 
-    private UserService service;
-
+    private final UserService service;
+    @Autowired
+    public AbstractUserController(UserService service) {
+        this.service = service;
+    }
     public List<User> getAll() {
         LOG.info("getAll");
         return service.getAll();
     }
 
     public User get(int id) {
-        LOG.info("get " + id);
+        LOG.info("get {}", id);
         return service.get(id);
     }
 
     public User create(User user) {
-        LOG.info("create " + user);
+        LOG.info("create {}", user);
         checkNew(user);
         return service.save(user);
     }
 
     public void delete(int id) {
-        LOG.info("delete " + id);
+        LOG.info("delete {}", id);
         service.delete(id);
     }
 
     public void update(User user, int id) {
-        LOG.info("update " + user);
+        LOG.info("update {}", user);
         checkIdConsistent(user, id);
         service.update(user);
     }
 
     public User getByMail(String email) {
-        LOG.info("getByEmail " + email);
+        LOG.info("getByEmail {}", email);
         return service.getByEmail(email);
     }
 }
